7a45b9e0c11d8a733c55566bea1de53054809d13
Natalija Ostojic <natalija@4it.rs>
2020-06-07 23:49:32 +0200

    Add static text before recording. Show animation after recoring started

--- Instagram filter/scripts/script.js
+++ Instagram filter/scripts/script.js
@@ -3,16 +3,16 @@ const Scene = require('Scene');
 const Patch = require('Patches');
 export const Diagnostics = require('Diagnostics');
 
-var textObj = Scene.root.child('Device').child('Camera').child('Focal Distance').child('canvas0').find('mibName');
+var textObj = Scene.root.child('Device').child('Camera').child('Focal Distance').child('miBNameCanvas').find('mibName');
 var imgIdx = Patch.getScalarValue('imageIdx');
-
+textObj.text = "";
 imgIdx.monitor().subscribe(val => {
     switch(val.newValue){
         case 0: 
-        textObj.text = "You are Agent J";
+        textObj.text = "Welcome Agent J";
         break;
         case 1: 
-        textObj.text = "You are Agent K";
+        textObj.text = "Welcome Agent K";
         break;
         case 2: 
         textObj.text = "You are Ballchinian";
@@ -27,10 +27,10 @@ imgIdx.monitor().subscribe(val => {
         textObj.text = "You are Jack Jeebs";
         break;
         case 6: 
-        textObj.text = "You are Laurel Weaver";
+        textObj.text = "Welcome Laurel Weaver";
         break;
         case 7: 
-        textObj.text = "You are Zed";
+        textObj.text = "Welcome Zed";
         break;
 
     }
